2b8c77a355db01877912fad5211987af3d9b93d9
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-11-17 16:28:01 -0500

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                //TODO: add semantic names
+                colorone: "#B4D6CD",
+                colortwo: "#FFDA76",
+                colorthree: "#FF8C9E",
+                colorfour: "#FF4E88"
+            }
+        },
+    },
+    plugins: [],
+}
+
