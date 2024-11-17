c7e8a0d7503ac80e1dd74807a47f41541d193ea8
mudasir <96320211+mud-ali@users.noreply.github.com>
2024-11-17 18:41:11 -0500

    home page done i guess

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,17 @@ const Home = () => {
                     <h2 className="text-lg">
                         BCA's Award Winning Literary Magazine
                     </h2>
+
+                    {/* TODO: Comment out if no new edition. Change URL to point to latest edition */}
+                    <div className="call-to-action mt-4">
+                        NEW! Fall Edition Out!
+                        <a
+                            className="mt-2 block border-2 border-black bg-colorone font-bold p-3 hover:scale-95 transition-all duration-150"
+                            href="https://drive.google.com/file/d/1SKPtVtFQBr6KqNrX_HzOx7EIaUrZl81s/view?usp=sharing"
+                        >
+                            Click to Read Now!
+                        </a>
+                    </div>
                 </section>
             </main>
         </>
