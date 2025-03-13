c5f48102bc84bcae1b2ead516b03a34c821b1867
hubhacker <shreyathirumala@gmail.com>
2025-03-12 20:18:20 -0400

    updated submit

--- src/pages/Submit.jsx
+++ src/pages/Submit.jsx
@@ -1,23 +1,27 @@
 import React, { useEffect } from "react";
 import Header from "../components/Header";
 
-const SUBMISSION_FORM_URL = "https://forms.gle/dREqwzxDpw7th3yu6";
+//const SUBMISSION_FORM_URL = "https://forms.gle/dREqwzxDpw7th3yu6";
+const SUBMISSION_FORM_URL = "";
+
 
 const Submit = () => {
 
     // dont want auto redirects? remove the following statement
     // you can also just set location = SUBMISSION_FORM_URL
     // if you dont want to overwrite history
-    setTimeout(
-        () => window.location.replace(SUBMISSION_FORM_URL),
-        1000
-    )
+    
+    
+    // setTimeout(
+    //     () => window.location.replace(SUBMISSION_FORM_URL),
+    //     1000
+    // )
 
     return (
         <>
             <Header />
             <main className="home-body">
-                <div className={`border-colorthree border-2 text-colorthree
+                <div className={`border-colorthree border-2 text-colorfour
                     bg-colorone text-center w-1/4 mx-auto p-4 rounded-md`}>
                     <h2
                         className="text-3xl"
@@ -26,10 +30,10 @@ const Submit = () => {
                     </h2>
                     <br />
                     <p className="">
-                        Contribute to the upcoming Valentine's Day Edition!
+                        Make sure to contribute to the upcoming Spring Edition!
                     </p>                    
                 </div>
-                <p className="text-center italic block my-3">
+                {/* <p className="text-center italic block my-3">
                     Redirecting.......
                     <br /> <br />
                     Not automatically redirected? Click&nbsp;
@@ -39,7 +43,7 @@ const Submit = () => {
                     >
                         here
                     </a>.
-                </p>
+                </p> */}
             </main>
         </>
     )
